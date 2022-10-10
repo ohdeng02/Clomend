a7e50fa96f3b1890d6e14becf98c55ac38846d4b
ZustPlay <93589346+ZustPlay@users.noreply.github.com>
2022-10-10 12:11:29 +0900

    git commit

---
+++ app/src/main/java/com/example/clomend/My_PageFragment.kt
@@ -0,0 +1,31 @@
+package com.example.clomend
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.example.clomend.My_PageFragment.Companion.spinner
+import com.example.clomend.databinding.FragmentMyPageBinding
+
+class My_PageFragment : Fragment() {
+    private var _binding : FragmentMyPageBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreatedView(inflater: LayoutInflater,
+                               container: ViewGroup?,
+                               savedInstanceState: Bundle?): View? {
+        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val spinner: Spinner = binding.sexSpinner
+        val sex = resources.getStringArray(R.array.sex)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item,sex )
+        binding.sexSpinner.adapter = adapter
+    }
+}
---
+++ app/src/main/java/com/example/clomend/login/SplashFragment.kt
@@ -0,0 +1,39 @@
+package com.example.clomend.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.example.clomend.R
+import com.example.clomend.databinding.FragmentSplashBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+class SplashFragment : Fragment() {
+    private var _binding:FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) : View? {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch(Dispatchers.IO){
+            delay(2000)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
