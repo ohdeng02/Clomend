a2b82215a39b69dbcbf330209ce71d8928134929
ZustPlay <93589346+ZustPlay@users.noreply.github.com>
2022-10-16 16:48:46 +0900

    파일 경로 수정

--- app/src/main/java/com/example/clomend/closet/CoordiMakeFragment.kt
+++ app/src/main/java/com/example/clomend/closet/CoordiMakeFragment.kt
@@ -1,10 +1,11 @@
-package com.example.clomend
+package com.example.clomend.closet
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.clomend.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ClothesAddFragment.newInstance] factory method to
+ * Use the [CoordiMakeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ClothesAddFragment : Fragment() {
+class CoordiMakeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class ClothesAddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_clothes_add, container, false)
+        return inflater.inflate(R.layout.fragment_coordi_make, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class ClothesAddFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ClothesAddFragment.
+         * @return A new instance of fragment CoordiMakeFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ClothesAddFragment().apply {
+            CoordiMakeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
