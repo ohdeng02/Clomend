937f10fd286b52253971e8355dd7dc7bf10b350b
ZustPlay <93589346+ZustPlay@users.noreply.github.com>
2022-10-18 22:15:01 +0900

    실행하면 스플래시 이미지 뜨는거 구현, 폰트 문제 해결, 하단 네비게이션 바 메인 액티비티에 구현

--- app/src/main/java/com/example/clomend/main/MainActivity.kt
+++ app/src/main/java/com/example/clomend/main/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.example.clomend.main
 
 import androidx.appcompat.app.AppCompatActivity
-
-private const val TAG_COORDI_BOOK= "coordi_book_fragment"
-private const val TAG_CALENDER = "calender_fragment"
-private const val TAG_CLOSET = "closet_fragment"
-private const val TAG_COMMUNITY = "community_fragment"
-private const val TAG_MY_PAGE = "my_page_fragment"
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.example.clomend.R
+import com.example.clomend.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val controller = findNavController(R.id.navigation_host)
+
+        NavigationUI.setupWithNavController(binding.bottomNavigation, controller)
+
+        controller.addOnDestinationChangedListener { _, destination, _ ->
+            if (arrayListOf(R.id.splashFragment, R.id.loginFragment,R.id.info_InputFragment,
+                R.id.clothesAddFragment).contains(destination.id)) {
+                binding.bottomNavigation.visibility = View.GONE
+            } else {
+                binding.bottomNavigation.visibility = View.VISIBLE
+            }
+        }
+    }
 }
